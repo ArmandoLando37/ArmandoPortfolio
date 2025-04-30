bcce2a408eaabaae9bb61dec22fe05b415c86a98
Armando Lando <tsarafara.37@gmail.com>
2025-04-30 15:17:16 +0300

    mettre les formulaire d'envoi de message fonctionnelle

---
+++ email.js
@@ -0,0 +1,16 @@
+
+emailjs.init("6lrzzcS-KHVxK5p72"); 
+
+// Écouteur d'événement pour la soumission du formulaire
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();  
+  // Envoi du formulaire via EmailJS
+  emailjs.sendForm("service_thsj447", "template_ndrri93", this)
+    .then(function(response) {
+
+      alert("✅ Message envoyé avec succès !");
+    }, function(error) {
+
+      alert("❌ Erreur : " + JSON.stringify(error));
+    });
+});
